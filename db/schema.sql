8ff6409af4a3820e24f4f66b5f84254de668623c
Stew5555 <104962519+Stew5555@users.noreply.github.com>
2022-11-26 19:20:01 -0800

    completed schema file

--- db/schema.sql
+++ db/schema.sql
@@ -2,4 +2,16 @@ CREATE TABLE department (
     id INTEGER AURO_INCREMENT, 
     name VARCHAR(30)
 )
+CREATE TABLE role (
+    id INTEGER AURO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL
+)
 
+CREATE TABLE emplyee(
+    id INTEGER AURO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER,
+    manger_id INTEGER
+)
