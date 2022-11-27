f7e832c29cad3424ba5c74954219a82dde760a94
Stew5555 <104962519+Stew5555@users.noreply.github.com>
2022-11-26 19:24:36 -0800

    creating roles and departments in seeds file

--- db/schema.sql
+++ db/schema.sql
@@ -1,16 +1,16 @@
 CREATE TABLE department (
-    id INTEGER AURO_INCREMENT, 
+    id INTEGER AURO_INCREMENT, PRIMARY KEY
     name VARCHAR(30)
 )
 CREATE TABLE role (
-    id INTEGER AURO_INCREMENT,
+    id INTEGER AURO_INCREMENT, PRIMARY KEY
     title VARCHAR(30),
     salary DECIMAL
 )
 
 CREATE TABLE emplyee(
     id INTEGER AURO_INCREMENT,
-    first_name VARCHAR(30) NOT NULL,
+    first_name VARCHAR(30) NOT NULL,PRIMARY KEY
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER,
     manger_id INTEGER
---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+INSERT INTO department
+  (name)
+VALUES
+  ('Engineering'),
+  ('Sales'),
+  ('Finance'),
+  ('Legal');
+
+  INSERT INTO role
+  (title, salary, department_id)
+VALUES
+  ('Software Engineer', 85000, 24),
+  ('Salesperson', 95000, 425),
+  ('Accountant', 10000, 46),
+  ('Lawyer', 20000, 431);
