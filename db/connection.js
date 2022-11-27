fb711c488044ef5cd5dd7cdb3b40b234ae0c3ba0
Stew5555 <104962519+Stew5555@users.noreply.github.com>
2022-11-26 19:15:08 -0800

    colpeted the connection file

---
+++ db/connection.js
@@ -0,0 +1,8 @@
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '60DGmO7!54rgXtR',
+    database: 'employee_db'
+});
