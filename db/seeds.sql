1277d6556bce107ef6f598cdebe5181e7daa1e6f
Stew5555 <104962519+Stew5555@users.noreply.github.com>
2022-11-26 19:28:41 -0800

    changed ids in seeds file

--- db/seeds.sql
+++ db/seeds.sql
@@ -9,15 +9,15 @@ VALUES
   INSERT INTO role
   (title, salary, department_id)
 VALUES
-  ('Software Engineer', 85000, 24),
-  ('Salesperson', 95000, 425),
-  ('Accountant', 10000, 46),
-  ('Lawyer', 20000, 431);
+  ('Software Engineer', 85000, 1),
+  ('Salesperson', 95000, 2),
+  ('Accountant', 100000, 3),
+  ('Lawyer', 200000, 4);
 
   INSERT INTO employee
   (first_name, last_name, role_id, manager_id)
 VALUES
-  ('Juan', 'Quila', 24, 4),
-  ('Jonathan', 'Christopher', 425, 3),
-  ('Josh', 'Meraz', 46, 1),
-  ('Estefany', 'Munoz', 431, 5);
+  ('Juan', 'Quila', 1, 4),
+  ('Jonathan', 'Christopher', 2, 3),
+  ('Josh', 'Meraz', 3, 1),
+  ('Estefany', 'Munoz', 4, 5);
