d138eb31cec68c608a2be1731d9edd1724f92f1f
Stew5555 <104962519+Stew5555@users.noreply.github.com>
2022-11-26 21:18:44 -0800

    adding a prompt func on index file

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ db.connect(err => {
     console.log('Database connected.');
     employee_tracker()
 })
+
+var employee_tracker = function () {
+    inquirer.prompt([{
+        type: 'list',
+        name: 'prompt',
+        message: 'What is it you wanted to do?',
+        choices: ['View All Department', 'View All Roles', 'View All Employees', 'Add A Department', 'Add A Role', 'Add An Employee', 'Update An Employee Role', 'Log Out']
+    }])
+}
