629404e9ffcd85c0c4f823d22e895de65356c3e5
Stew5555 <104962519+Stew5555@users.noreply.github.com>
2022-11-26 19:27:22 -0800

    completed seeds file

--- db/seeds.sql
+++ db/seeds.sql
@@ -13,3 +13,11 @@ VALUES
   ('Salesperson', 95000, 425),
   ('Accountant', 10000, 46),
   ('Lawyer', 20000, 431);
+
+  INSERT INTO employee
+  (first_name, last_name, role_id, manager_id)
+VALUES
+  ('Juan', 'Quila', 24, 4),
+  ('Jonathan', 'Christopher', 425, 3),
+  ('Josh', 'Meraz', 46, 1),
+  ('Estefany', 'Munoz', 431, 5);
